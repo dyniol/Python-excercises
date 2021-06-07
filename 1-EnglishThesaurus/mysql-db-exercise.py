b4d876ed294925b95b2847f4e867def9779551d4
dyniol <dyniol1@gmail.com>
2021-06-07 09:55:47 +0200

    .

--- 1-EnglishThesaurus/mysql-db-exercise.py
+++ 1-EnglishThesaurus/mysql-db-exercise.py
@@ -1,21 +1,54 @@
+import re
 import mysql.connector
+from difflib import get_close_matches
 
-con = mysql.connector.connect(
+def connect_to_database():
+    con = mysql.connector.connect(
     user = "ardit700_student",
     password = "ardit700_student",
     host = "108.167.140.122",
-    database = "ardit700_pm1database"
-)
+    database = "ardit700_pm1database")
+    return con
 
-cursor = con.cursor()
+def search_for_word(query, con):
+    cursor = con.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return results
 
-word = input("Enter a word: ")
+def translate(word, con):
+    query = "SELECT * FROM Dictionary WHERE Expression = '%s'" % word
+    results = search_for_word(query, con)
+    # unlike in previous example mysql query for a word are case insensitive
+    # so we don't need to check for lower, upper and title
+    if results:
+        return [ result[1] for result in results]
+    else:
+        # gather all the dictionary words and find the close matches
+        query = "SELECT * FROM Dictionary"
+        results = search_for_word(query, con)
+        list_to_be_compared = [val[0] for val in results]
 
-query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % word)
-results = cursor.fetchall()
+        if len(get_close_matches(word, list_to_be_compared, n = 3, cutoff = 0.8)) > 0:
+            yn = input("Did you mean %s instead? ( Y/N ): " % get_close_matches(word, list_to_be_compared, n = 3, cutoff = 0.8)[0] )
+            if yn.upper()[0] == 'Y':
+                suggested_word = get_close_matches(word, list_to_be_compared, n = 3, cutoff = 0.8)[0]
+                query = "SELECT * FROM Dictionary WHERE Expression = '%s'" % suggested_word
+                results = search_for_word(query, con)
+                return [ result[1] for result in results]
+            elif yn.upper()[0] == 'N':
+                return "The word doesn't exist. Please double check it."
+            else:
+                return "We didn't understand your entry."
+        else:
+            return "The word doesn't exist. Please double check it."
 
-if results:
-    for result in results:
-        print(result[1])
+word = input("Enter word: ")
+con = connect_to_database()
+output = translate(word, con)
+
+if type(output) == list:
+    for item in output:
+        print(item)
 else:
-    print("No word found.")
+    print(output)
